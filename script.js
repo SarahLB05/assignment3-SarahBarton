b6389cdf3c10029dea69c4645c1eafa42eb3a2a6
Sarah B <152326347+SarahLB05@users.noreply.github.com>
2026-05-01 22:57:51 -0400

    slideshow

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ toggleBtn.addEventListener("click", () => {
     ? "Switch to Day Mode"
     : "Switch to Night Mode";
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
