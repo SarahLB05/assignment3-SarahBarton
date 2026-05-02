6ec4301920d1f8fb93fa5810f8bbde96bee65afa
Sarah B <152326347+SarahLB05@users.noreply.github.com>
2026-05-01 20:21:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const body = document.body;
 
 toggleBtn.addEventListener("click", () => {
   body.classList.toggle("night-mode");
+
   toggleBtn.textContent = body.classList.contains("night-mode")
     ? "Switch to Day Mode"
     : "Switch to Night Mode";
